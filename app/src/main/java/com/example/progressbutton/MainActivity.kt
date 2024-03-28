a0a25b94a01a6d0d3b671a7211fd74bff73060d5
olehshyker <ms.shyker@gmail.com>
2024-03-28 09:44:43 +0200

    Added usage example

--- app/src/main/java/com/example/progressbutton/MainActivity.kt
+++ app/src/main/java/com/example/progressbutton/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.progressbutton
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -12,31 +14,49 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.progressbutton.ui.theme.AppTheme
 import com.olehsh.progressapp.R
 import com.olehsh.progressbutton.ProgressButton
+import com.olehsh.progressbutton.ProgressButtonDefaults
 import com.olehsh.progressbutton.ProgressChangeMode
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
       AppTheme {
-        SampleApp()
+        Scaffold(
+          topBar = {
+            TopAppBar(
+              title = {
+                Text(text = stringResource(id = R.string.app_name))
+              },
+              backgroundColor = MaterialTheme.colors.primary,
+              contentColor = Color.White,
+              elevation = 2.dp
+            )
+          },
+          content = { padding ->
+            SampleApp(
+              modifier = Modifier.padding(padding)
+            )
+          }
+        )
       }
     }
   }
@@ -48,61 +68,61 @@ fun SampleApp(modifier: Modifier = Modifier) {
     modifier = modifier
       .fillMaxSize()
       .padding(16.dp)
-      .background(Color.White)
+      .background(Color.White),
+    horizontalAlignment = Alignment.CenterHorizontally
   ) {
-    Text(
-      text = stringResource(id = R.string.app_name),
-      modifier = Modifier.align(Alignment.CenterHorizontally),
-      fontFamily = FontFamily(Font(R.font.montserrat_semibold)),
-      fontSize = MaterialTheme.typography.h5.fontSize,
-      style = TextStyle.Default.copy(color = Color.Black)
-    )
+    StartButtonExample()
 
     Spacer(modifier = Modifier.height(16.dp))
+  }
+}
 
-    val progress = remember { mutableFloatStateOf(0f) }
-
-    ProgressButton(
-      modifier = Modifier
-        .align(Alignment.CenterHorizontally)
-        .fillMaxWidth()
-        .height(64.dp),
-      progress = progress.floatValue,
-      textStyle = TextStyle.Default.copy(fontSize = 20.sp),
-      buttonText = if (progress.floatValue < 100) "Press to update progress" else "Reset progress",
-      fontResId = R.font.montserrat_semibold,
-      progressChangeMode = ProgressChangeMode.AnimatedChange(),
-      onClick = {
-        if (progress.floatValue > 100) {
-          progress.floatValue = 0f
-        } else {
-          progress.floatValue += 10f
-        }
-      }
-    )
-
-    Spacer(modifier = Modifier.height(16.dp))
-
-    ProgressButton(
-      modifier = Modifier
-        .align(Alignment.CenterHorizontally)
-        .fillMaxWidth()
-        .height(64.dp),
-      progress = progress.floatValue,
-      border = BorderStroke(2.dp, Color.Blue),
-      textStyle = TextStyle.Default.copy(fontSize = 20.sp),
-      buttonText = if (progress.floatValue < 100) "Press to update progress" else "Reset progress",
-      fontResId = R.font.montserrat_semibold,
-      progressChangeMode = ProgressChangeMode.AnimatedChange(),
-      onClick = {
-        if (progress.floatValue > 100) {
-          progress.floatValue = 0f
-        } else {
-          progress.floatValue += 10f
-        }
-      }
+@Composable
+fun StartButtonExample() {
+  val animatedProgress = remember {
+    Animatable(
+      ProgressButtonDefaults.MIN_PROGRESS
     )
   }
+
+  val scope = rememberCoroutineScope()
+
+  val buttonText = when (animatedProgress.value) {
+    0f -> {
+      stringResource(id = R.string.txt_start)
+    }
+
+    100f -> {
+      stringResource(id = R.string.txt_done)
+    }
+
+    else -> {
+      stringResource(id = R.string.txt_downloading)
+    }
+  }
+
+  ProgressButton(
+    modifier = Modifier
+      .fillMaxWidth()
+      .height(64.dp),
+    progress = animatedProgress.value,
+    textStyle = TextStyle.Default.copy(fontSize = 20.sp),
+    buttonText = buttonText,
+    fontResId = R.font.montserrat_semibold,
+    progressChangeMode = ProgressChangeMode.InstantChange,
+    onClick = {
+      scope.launch {
+        if (animatedProgress.value == ProgressButtonDefaults.MAX_PROGRESS) {
+          animatedProgress.snapTo(ProgressButtonDefaults.MIN_PROGRESS)
+        } else {
+          animatedProgress.animateTo(
+            ProgressButtonDefaults.MAX_PROGRESS,
+            tween(4000, easing = LinearEasing)
+          )
+        }
+      }
+    }
+  )
 }
 
 @Preview(showBackground = true)
